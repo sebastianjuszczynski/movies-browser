22ba40ac743ac43ac254d77a73fe950057779fca
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-29 18:37:38 +0000

    GlobalStyle added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing:border-box;
+    };
+    *, ::after, ::before {
+        box-sizing: inherit;
+    };
+    body {
+        background: ${({ theme }) => theme.colors.Mercury};
+    };
+`;
