9aed57bb86b7ac3b11086ea4bd55542e811486ab
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-29 20:05:43 +0000

    activePage, setActivePage and selector added plus simple styles

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    fetchPopularMovies,
+    selectPopularMovies,
+    selectLoading,
+} from "./moviesSlice";
 
 const MoviesPage = () => {
+    const popularMovies = useSelector(selectPopularMovies);
+    const isLoading =useSelector(selectLoading);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchPopularMovies());
+    }, [])
+
     return (
-        <p>Movies Page</p>
+        <div>
+            {isLoading ? "loading" : popularMovies.map(movie => 
+                <p key={movie.id}>{movie.title}</p>)}
+        </div>
     );
 };
 
