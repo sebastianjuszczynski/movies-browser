5acba920b80ba5f225b434761e4e086d2fb9575f
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-30 20:56:41 +0000

    see below...

--- src/features/BottomNavigation/index.js
+++ src/features/BottomNavigation/index.js
@@ -1,35 +1,39 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {selectActivePage, setActivePage} from "../movies/MoviesPage/moviesSlice";
-import {Button} from "./styled";
+import { selectActivePage, setActivePage, selectNumberPages } from "../movies/MoviesPage/moviesSlice";
+import { Container, Button, PageCounter, Number } from "./styled";
 
 const BottomNavigation = () => {
     const activePage = useSelector(selectActivePage);
+    const numberPages = useSelector(selectNumberPages);
     const dispatch = useDispatch();
 
     return (
-        <div>
+        <Container>
             <Button
                 disabled={activePage === 1}
                 onClick={() => dispatch(setActivePage(1))}
-            >{"<<"}
+            >{"< First"}
             </Button>
             <Button
                 disabled={activePage === 1}
                 onClick={() => dispatch(setActivePage(activePage - 1))}
-            >{"<"}
+            >{"< Previous"}
             </Button>
+            <PageCounter>
+                Page <Number>{activePage}</Number> of <Number>{numberPages}</Number>
+            </PageCounter>
             <Button
                 disabled={activePage === 500}
                 onClick={() => dispatch(setActivePage(activePage + 1))}
-            >{">"}
+            >{"Next >"}
             </Button>
             <Button
                 disabled={activePage === 500}
                 onClick={() => dispatch(setActivePage(500))}
-            >{">>"}
+            >{"Last >"}
             </Button>
-        </div>
+        </Container>
     );
 };
 
--- src/features/BottomNavigation/styled.js
+++ src/features/BottomNavigation/styled.js
@@ -1,5 +1,31 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+    width: fit-content;
+    margin: 40px auto 100px;
+`;
 export const Button = styled.button`
-    padding: 10px;
+    margin: 0 12px;
+    padding: 8px 16px 8px 16px;
+    background: ${({theme}) => theme.colors.pattensBlue};
+    border-radius: 5px;
+    color: ${({ theme }) => theme.colors.mineShaft};
+    border: none;
+    cursor: pointer;
+
+    &:disabled {
+        background-color: ${({theme}) => theme.colors.cornsilk};
+    }
+`;
+export const PageCounter = styled.div`
+    margin: 0 24px;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${({theme}) => theme.colors.waterloo};
+`;
+export const Number = styled.span`
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 1.5;
+    color: ${({theme}) => theme.colors.woodsmoke};
 `;
