ec2e1c4ff4caf5102d41b01040927abc6e99cc65
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-30 20:51:56 +0000

    fixed issue with grid

--- src/features/movies/MoviesPage/styled.js
+++ src/features/movies/MoviesPage/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const MoviesContainer = styled.div`
     display: grid;
-    grid-template-columns: repeat()(4, 1fr);
+    grid-template-columns: repeat(4, 1fr);
     grid-gap:24px;
     margin: 24px auto 40px;
     width: 1368px;
