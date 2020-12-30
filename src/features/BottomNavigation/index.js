d5be14ab975bba9b2a30f5ba0787b58bf4c97bce
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-30 23:00:46 +0000

    saving page number in url added on button click

--- src/features/BottomNavigation/index.js
+++ src/features/BottomNavigation/index.js
@@ -1,23 +1,30 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectActivePage, setActivePage, selectNumberPages } from "../movies/MoviesPage/moviesSlice";
+import { useReplacePageParameter } from "../pageParameters";
 import { Container, Button, PageCounter, Number } from "./styled";
 
 const BottomNavigation = () => {
     const activePage = useSelector(selectActivePage);
     const numberPages = useSelector(selectNumberPages);
     const dispatch = useDispatch();
+    const replacePageParameter = useReplacePageParameter();
+
+    const onButtonClick = (page) => {
+        replacePageParameter("page", page);
+        dispatch(setActivePage(page));
+    };
 
     return (
         <Container>
             <Button
                 disabled={activePage === 1}
-                onClick={() => dispatch(setActivePage(1))}
+                onClick={() => onButtonClick(1)}
             >{"< First"}
             </Button>
             <Button
                 disabled={activePage === 1}
-                onClick={() => dispatch(setActivePage(activePage - 1))}
+                onClick={() => onButtonClick(activePage - 1)}
             >{"< Previous"}
             </Button>
             <PageCounter>
@@ -25,12 +32,12 @@ const BottomNavigation = () => {
             </PageCounter>
             <Button
                 disabled={activePage === numberPages}
-                onClick={() => dispatch(setActivePage(activePage + 1))}
+                onClick={() => onButtonClick(activePage + 1)}
             >{"Next >"}
             </Button>
             <Button
                 disabled={activePage === numberPages}
-                onClick={() => dispatch(setActivePage(500))}
+                onClick={() => onButtonClick(numberPages)}
             >{"Last >"}
             </Button>
         </Container>
