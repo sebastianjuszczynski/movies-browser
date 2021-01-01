81663f6995ee5297b264f25f57c4e084ecef9a80
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-01 21:49:58 +0000

    media queries added to Star, Rate and Votes

--- src/common/Ratings/styled.js
+++ src/common/Ratings/styled.js
@@ -8,13 +8,27 @@ export const Container = styled.div`
 export const Star = styled.img`
     width: 24px;
     height: 22.87px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    width: 16px;
+    height: 15.25px;
+  }
 `;
 export const Rate = styled.span`
     margin: 0 12px;
     font-weight: 600;
     font-size: 16px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 13px;
+    margin: 0 7px;
+  }
 `;
 export const Votes = styled.span`
     font-size: 16px;
-    color: ${({theme}) => theme.colors.waterloo};
+    color: ${({ theme }) => theme.colors.waterloo};
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 13px;
+  }
 `;
