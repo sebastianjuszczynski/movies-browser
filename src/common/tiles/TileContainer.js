1260a6d052bd104342ac7291a5a09b7614dd0dab
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-01 20:58:15 +0000

    TileContainer changed to MovieContainer

--- src/common/tiles/TileContainer.js
+++ src/common/tiles/TileContainer.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
-export const TileContainer = styled.div`
+export const MovieContainer = styled.div`
     display: grid;
     max-width: 1368px;
     margin: 24px auto 40px;
     grid-template-columns: repeat(auto-fill, 320px);
     grid-gap: 24px;
     justify-content: center;
+`;
+export const PeopleContainer = styled(MovieContainer)`
+    grid-template-columns: repeat(6, 1fr);
 `;
