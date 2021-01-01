fb974f8584432f764c85a3e240201a066af9e139
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-01 16:35:48 +0000

    getPopularMovies changed to getApiData

--- src/features/movies/MoviesPage/moviesSaga.js
+++ src/features/movies/MoviesPage/moviesSaga.js
@@ -1,5 +1,7 @@
 import { takeEvery, call, put, delay } from "redux-saga/effects";
-import { getPopularMovies } from "./getPopularMovies";
+import { getApiData } from "../../getApiData";
+
+import store from "./../../../store";
 import {
     setActivePage,
     fetchPopularMoviesError,
@@ -7,9 +9,10 @@ import {
 } from "./moviesSlice";
 
 function* fetchPopularMoviesHandler() {
+    const page = store.getState().movies.activePage;
     try {
         yield delay(500);
-        const popularMovies = yield call(getPopularMovies);
+        const popularMovies = yield call(() => getApiData(`https://api.themoviedb.org/3/movie/popular?api_key=db28f87903ed5f61125cde9d85f339b2&language=en-US&page=${page}`));
         yield put(fetchPopularMoviesSuccess(popularMovies));
     } catch (error) {
         yield put(fetchPopularMoviesError());
