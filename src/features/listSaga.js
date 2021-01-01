8236b5ef12a0363d3ca387733bb7c8938f0da234
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-01 18:13:25 +0000

    moviesSaga renamed to listSaga and moved to features

---
+++ src/features/listSaga.js
@@ -0,0 +1,28 @@
+import { takeEvery, call, put, delay } from "redux-saga/effects";
+import { getApiData } from "./getApiData";
+import apiKey from "../common/apiKey";
+import language from "../common/language";
+import store from "../store";
+import {
+    setActivePage,
+    fetchListError,
+    fetchListSuccess,
+} from "./listSlice";
+
+function* fetchListHandler() {
+    const page = store.getState().list.activePage;
+    const activePath = store.getState().list.activePath;
+    try {
+        yield delay(500);
+        const data = yield call(() => getApiData(`https://api.themoviedb.org/3${activePath}?api_key=${apiKey}&language=${language}&page=${page}`
+        ));
+        yield put(fetchListSuccess(data));
+    } catch (error) {
+        yield put(fetchListError());
+        yield call(alert, "Something went wrong...");
+    }
+};
+
+export function* watchSetActivePages() {
+    yield takeEvery(setActivePage.type, fetchListHandler);
+};
