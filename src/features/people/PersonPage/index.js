9f7b2079d046f595b9eff47852c8166879f6f859
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-01 20:40:45 +0000

    PersonPage created

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Loading from "../../../common/Loading";
+import {
+    setActivePath,
+    selectLoading,
+    selectItemData,
+    setItemId,
+    selectExtraData
+} from "../../itemSlice";
+import language from "./../../../common/language";
+import apiKey from "./../../../common/apiKey";
 
 const PersonPage = () => {
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const personData = useSelector(selectItemData);
+    const castCrewData = useSelector(selectExtraData);
+    const isLoading = useSelector(selectLoading);
+
+    useEffect(() => {
+        dispatch(setActivePath({
+            path1: `https://api.themoviedb.org/3/person/${id}?api_key=${apiKey}&language=${language}`,
+            path2: `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${apiKey}&language=${language}`,
+        }));
+        dispatch(setItemId(id));
+    }, [id])
+
     return (
-        <p>Person Page</p>
-    );
+        <>
+            {isLoading ?
+                <Loading />
+                :
+                <>
+                    <div> {personData.name}</div>
+                    <div>tile</div>
+                    <div>cast</div>
+                    <div>crew</div>
+                </>
+                }
+        </>
+    )
 };
 
 export default PersonPage;
