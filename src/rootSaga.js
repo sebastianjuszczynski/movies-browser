840e9717875983cf2edd17551209fd0d9386a3d2
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-01 15:21:33 +0000

    changes to get reusable version

---
+++ src/rootSaga.js
@@ -0,0 +1,10 @@
+import { all } from "redux-saga/effects";
+// import { watchSetMovie } from "./features/movies/MoviePage/movieSaga";
+import { watchSetActivePages } from "./features/movies/MoviesPage/moviesSaga";
+
+export default function* rootSaga() {
+    yield all([
+        watchSetActivePages(),
+        // watchSetMovie(),
+    ]);
+};
--- src/store.js
+++ src/store.js
@@ -1,16 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./features/movies/MoviesPage/moviesSlice";
+// import movieReducer from "./features/movies/MoviePage/movieSlice";
 import createSagaMiddleware from "redux-saga";
-import { watchSetActivePages } from "./features/movies/MoviesPage/moviesSaga";
+import rootSaga from "./rootSaga";
 
 const SagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         movies: moviesReducer,
+        // movie: movieReducer,
     },
     middleware: [SagaMiddleware],
 });
-SagaMiddleware.run(watchSetActivePages);
+SagaMiddleware.run(rootSaga);
 
 export default store;
