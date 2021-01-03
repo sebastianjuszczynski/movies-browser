4f7fd9ff0ab22b9ca57a846f94915a1a6c36e60a
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-03 17:16:53 +0000

    fixed issue with pageParameters

--- src/features/pageParameters.js
+++ src/features/pageParameters.js
@@ -1,23 +1,23 @@
 import { useHistory, useLocation } from "react-router-dom";
-
 export const usePageParameter = paramName => {
     const location = useLocation();
     return (new URLSearchParams(location.search)).get(paramName);
 };
-
-export const useReplacePageParameter = () => {
+export const useReplacePageParameters = () => {
     const history = useHistory();
     const location = useLocation();
-
     const searchParams = new URLSearchParams(location.search);
+    const replacePageParameters = (parameters) => {
+        parameters.forEach(({ key, value }) => {
+            if (value === "") {
+                searchParams.delete(key);
+            } else {
+                searchParams.set(key, value);
+            }
+        });
 
-    const replacePageParemeter = ( key, value ) => {
-        if (value === "") {
-            searchParams.delete(key);
-        } else {
-            searchParams.set(key, value);
-        }
-        history.push(`${location.pathname}?${searchParams.toString()}`);
+        history.push(`${location.pathname.includes("movie") ? "/movies" : "/people"}?${searchParams.toString()}`);
     };
-    return replacePageParemeter;
-}
+
+    return replacePageParameters;
+};
