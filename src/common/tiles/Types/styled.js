fc5d0a6b48e45332bdb02a8e37054edf66248b63
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-04 20:38:53 +0000

    Types styled

--- src/common/tiles/Types/styled.js
+++ src/common/tiles/Types/styled.js
@@ -6,19 +6,19 @@ export const TypesWrapper = styled.div`
 `;
 export const TypeBox = styled.div`
     padding: 8px 16px;
-    height: 36px;
-    background-color: ${({theme}) => theme.colors.cornsilk};
+    background-color: ${({ theme }) => theme.colors.cornsilk};
     border-radius: 5px;
     margin: 0 8px 8px 0;
     font-size: 14px;
-    line-height: 140%;
-    color: ${({theme}) => theme.colors.woodsmoke};
+    line-height: 1.4;
+    color: ${({ theme }) => theme.colors.woodsmoke};
     flex-grow: 0;
+  
+    @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}) {
+  }
 `;
 export const BigTypeBox = styled(TypeBox)`
     font-size: 14px;
-    line-height: 100%;
-    width: 81px;
-    height: 30px;
+    line-height: 1;
     margin-right: 8px;
 `;
