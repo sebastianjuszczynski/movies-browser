d05a138f42b36ec9c677805caa3e92586732686c
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-04 16:20:28 +0000

    saga and slice added for mini search

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -2,9 +2,11 @@ import { all } from "redux-saga/effects";
 import { watchSetItemId } from "./features/itemSaga";
 import { watchSetActivePath } from "./features/listSaga";
 import { watchfetchCommon } from "./common/commonSaga";
+import { watchSetActiveSearchPath } from "./features/Search/searchSaga";
 
 export default function* rootSaga() {
     yield all([
+        watchSetActiveSearchPath(),
         watchSetActivePath(),
         watchSetItemId(),
         watchfetchCommon(),
