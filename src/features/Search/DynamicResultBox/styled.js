0390fe69ece69aa9d01e5c57dd4fdd6582bd7819
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-08 18:34:11 +0000

    max-height added to Search wrapper

--- src/features/Search/DynamicResultBox/styled.js
+++ src/features/Search/DynamicResultBox/styled.js
@@ -5,7 +5,7 @@ export const Wrapper = styled.div`
     flex-direction: column;
     position: absolute;
     width: 100%;
-    height: 500px;
+    max-height: 500px;
     overflow: auto;
     background-color: ${({ theme }) => theme.colors.white};
     color: ${({ theme }) => theme.colors.black};
@@ -16,7 +16,7 @@ export const Wrapper = styled.div`
         width: 20px;
     }
     ::-webkit-scrollbar-track {
-        border-radius:10px;
+        border-radius: 10px;
     }
     ::-webkit-scrollbar-thumb {
         background: ${({ theme }) => theme.colors.cornsilk};
