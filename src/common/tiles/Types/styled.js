d2b75b74cb4354696748500573ceb0c5b88dd3ff
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-08 17:59:03 +0000

    added media queries for Types

--- src/common/tiles/Types/styled.js
+++ src/common/tiles/Types/styled.js
@@ -21,4 +21,15 @@ export const BigTypeBox = styled(TypeBox)`
     font-size: 14px;
     line-height: 1;
     margin-right: 8px;
+
+    @media (max-width: ${({theme}) => theme.breakpoints.bigMobile}px) {
+        font-size: 12px;
+        font-weight: 400;
+        padding: 6px 12px;
+  }
+    @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+        font-size: 10px;
+        font-weight: 400;
+        padding: 4px 8px;
+  }
 `;
