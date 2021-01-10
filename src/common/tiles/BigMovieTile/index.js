ee6304dced05d6fcbece9b2529ac6b07e47415fd
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-10 18:27:49 +0000

    fixed BigMovieTile for mobiles

--- src/common/tiles/BigMovieTile/index.js
+++ src/common/tiles/BigMovieTile/index.js
@@ -27,45 +27,75 @@ const BigMovieTile = ({
 }) => {
     return (
         <BigMovieWrapper as="div">
+          <DesktopWrapper>
             <BigMovieImage
-                src={poster_path
+              src={
+                poster_path
+                  ? `https://image.tmdb.org/t/p/w342${poster_path}`
+                  : noPosterImage
+              }
+              alt={`Plakat filmu ${title}`}
+            />
+              {title && <BigTileHeader>{title}</BigTileHeader>}
+              <DetailsWrapper>
+                {production_countries && (
+                  <Detail>
+                    <MarkedDetail>Production: </MarkedDetail>
+                    {production_countries.map((country) => `${country.name}, `)}
+                  </Detail>
+                )}
+                {release_date && (
+                  <Detail>
+                    <MarkedDetail>Release date: </MarkedDetail> {release_date}
+                  </Detail>
+                )}
+              </DetailsWrapper>
+              <Types big={true} genre_ids={genre_ids} />
+              <Ratings
+                vote_average={vote_average}
+                vote_count={vote_count}
+                big={true}
+              />
+              {overview && <Description>{overview}</Description>}
+          </DesktopWrapper>
+          <MobileWrapper>
+            <TopWrapper>
+              <BigMovieImage
+                src={
+                  poster_path
                     ? `https://image.tmdb.org/t/p/w342${poster_path}`
                     : noPosterImage
                 }
                 alt={`Plakat filmu ${title}`}
-            />
-            <BigDetailsContainer>
-                {title &&
-                    <BigTileHeader>
-                        {title}
-                    </BigTileHeader>
-                }
+              />
+              <BigDetailsContainer>
+                {title && <BigTileHeader>{title}</BigTileHeader>}
                 <DetailsWrapper>
-                    {production_countries &&
-                        <Detail>
-                            <MarkedDetail>Production: </MarkedDetail> {production_countries.map(country => `${country.name }`)}
-                        </Detail>
-                    }
-                    {release_date &&
-                        <Detail>
-                            <MarkedDetail>Release date: </MarkedDetail> {release_date}
-                        </Detail>
-                    }
+                  {production_countries && (
+                    <Detail>
+                      <MarkedDetail>Production: </MarkedDetail>
+                      {production_countries.map((country) => `${country.name}, `)}
+                    </Detail>
+                  )}
+                  {release_date && (
+                    <Detail>
+                      <MarkedDetail>Release date: </MarkedDetail> {release_date}
+                    </Detail>
+                  )}
                 </DetailsWrapper>
-                <Types big={true} genre_ids={genre_ids} />
+                <Types genre_ids={genre_ids} />
                 <Ratings
-                    vote_average={vote_average}
-                    vote_count={vote_count}
-                    big={true}
+                  vote_average={vote_average}
+                  vote_count={vote_count}
+                  big={true}
                 />
-                {overview &&
-                    <Description>
-                        {overview}
-                    </Description>
-                }
-            </BigDetailsContainer>
-        </BigMovieWrapper >
-    );
-};
-
-export default BigMovieTile;
+              </BigDetailsContainer>
+            </TopWrapper>
+            {overview && <Description>{overview}</Description>}
+          </MobileWrapper>
+        </BigMovieWrapper>
+      );
+  };
+  
+  export default BigMovieTile;
+  
