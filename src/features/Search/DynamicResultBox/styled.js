4ad6dfdf2d1680ab8bb941bef6d373a717fa779e
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-10 14:54:27 +0000

    colors for darkTheme changed

--- src/features/Search/DynamicResultBox/styled.js
+++ src/features/Search/DynamicResultBox/styled.js
@@ -7,8 +7,8 @@ export const Wrapper = styled.div`
     width: 100%;
     max-height: 500px;
     overflow: auto;
-    background-color: ${({theme}) => theme.unchangeableColors.white};
-    color: ${({theme}) => theme.unchangeableColors.black};
+    background-color: ${({theme}) => theme.colors.tileBackground};
+    color: ${({theme}) => theme.colors.text};
     border-radius: 0 0 10px 10px;
     border: 1px solid ${({ theme }) => theme.colors.typeBox};
     border-top: none;
@@ -20,7 +20,7 @@ export const Wrapper = styled.div`
         border-radius: 10px;
     }
     ::-webkit-scrollbar-thumb {
-        background: ${({ theme }) => theme.colors.typeBox};
+        background: ${({theme}) => theme.unchangeableColors.cornsilk};
         border-radius: 10px;
     }
 `;
