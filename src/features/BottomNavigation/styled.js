ee220a30e6889ee6bfdd9c97caccc48f23879e00
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-10 17:41:03 +0000

    animation for hover effect on BottomNavigation

--- src/features/BottomNavigation/styled.js
+++ src/features/BottomNavigation/styled.js
@@ -34,12 +34,16 @@ export const Button = styled.button`
       background: ${({ theme }) => theme.colors.disabledNavButton};
       cursor: unset;
     }
+    &:hover {
+      transform: scale(1.05);
+      transition: transform 0.4s;
+    }
   @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
     display: flex;
     padding: 8px 12px;
     margin: 0 4px;
   }
-  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     margin: 0 4px;
   }
 `;
@@ -55,9 +59,9 @@ export const Number = styled.span`
     font-weight: 600;
     font-size: 16px;
     line-height: 150%;
-    color: ${({theme}) => theme.colors.text};
+    color: ${({ theme }) => theme.colors.text};
 
-    @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     font-size: 10px;
   }
 `;
@@ -66,10 +70,10 @@ export const StyledArrowLeft = styled(leftArrow)`
   margin-right: 8px;
   height: 11px;
   width: 7px;
-  @media (max-width: ${({theme}) => theme.breakpoints.bigMobile}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
     margin: 0 2px;
   }
-  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     height: 8px;
     width: 5px;
   }
@@ -79,22 +83,22 @@ export const StyledArrowRight = styled(rightArrow)`
   height: 11px;
   width:7px;
   margin-left: 8px;
-  @media (max-width: ${({theme}) => theme.breakpoints.bigMobile}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
     margin: 0 2px;
   }
-  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     height:8px;
     width: 5px;
   }
 `;
 
 export const DoubleArrow = styled.div`
-  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     display: flex;
   }
-`; 
+`;
 export const SingleArrow = styled.div`
-  @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
     display: flex;
     height: 8px;
   }
