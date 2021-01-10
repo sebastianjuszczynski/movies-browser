bded122ffac9bc4ced2258eb19fbe0a1293a3090
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-10 15:15:08 +0000

    Types styles updated for clickable and enabled props

--- src/common/tiles/Types/styled.js
+++ src/common/tiles/Types/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const TypesWrapper = styled.div`
     display: flex;
@@ -11,26 +11,40 @@ export const TypeBox = styled.div`
     border-radius: 5px;
     margin: 0 8px 8px 0;
     font-size: 14px;
-    line-height: 1.4;
+    line-height: 1;
     color: ${({ theme }) => theme.colors.text};
     flex-grow: 0;
-  
-    @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}) {
-  }
 `;
 export const BigTypeBox = styled(TypeBox)`
-    font-size: 14px;
-    line-height: 1;
-    margin-right: 8px;
+  line-height: 140%;
+  margin-right: 8px;
 
-    @media (max-width: ${({theme}) => theme.breakpoints.bigMobile}px) {
-        font-size: 12px;
-        font-weight: 400;
-        padding: 6px 12px;
-  }
-    @media (max-width: ${({theme}) => theme.breakpoints.mobile}px) {
-        font-size: 10px;
-        font-weight: 400;
-        padding: 4px 8px;
-  }
+  ${({ clickable }) => clickable && css`
+    cursor: pointer;
+    background-color: ${({ theme }) => theme.colors.activeNavButton};
+
+    &:hover{
+      filter: brightness(120%);
+    }
+
+    &:active{
+      filter: brightness(140%);
+    }
+  `}
+
+  ${({ enabled }) => enabled && css`
+    background: ${({ theme }) => theme.colors.backButton};
+    filter: brightness(160%);
+  `}
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
+      font-size: 12px;
+      font-weight: 400;
+      padding: 6px 12px;
+    }
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+      font-size: 10px;
+      font-weight: 400;
+      padding: 4px 8px;
+    }
 `;
