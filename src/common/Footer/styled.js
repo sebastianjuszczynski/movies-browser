e84bd0e3b31d23c3d56ed02b1712bcca40fd9873
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-10 14:29:24 +0000

    Footer styles added

--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -1,7 +1,30 @@
-export const Footer = styled.footer`
+import styled from "styled-components";
+
+export const Wrapper = styled.footer`
+  display: flex;
+  align-items: center;
   margin-top: auto;
-  background: ${({theme}) => theme.unchangeableColors.black};
-  color: ${({theme}) => theme.unchangeableColors.white};
+  background: ${({ theme }) => theme.unchangeableColors.black};
+  color: ${({ theme }) => theme.unchangeableColors.white};
   height: 50px;
-  max-width: 100vw;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+`;
+export const SwitchButtonBox = styled.div`
+  width: 90px;
+  padding: 5px;
+  margin: auto auto;
+  border-radius: 5px;
+  transition: all 0.5s;
+  background: ${({ theme }) => theme.colors.text};
+`;
+export const SwitchButton = styled.button`
+  display: block;
+  background: ${({ theme }) => theme.colors.background};
+  border-radius: 5px;
+  height: 20px;
+  width: 20px;
+  transition: all 0.5s;
+  transform: ${({ isNormalTheme }) => isNormalTheme ? "unset" : "translateX(300%)"};;
 `;
