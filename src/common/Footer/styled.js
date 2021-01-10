5fa211c6823313d333f672524a3ee051680524ae
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-10 14:26:10 +0000

    Footer styles moved to Footer folder

---
+++ src/common/Footer/styled.js
@@ -0,0 +1,7 @@
+export const Footer = styled.footer`
+  margin-top: auto;
+  background: ${({theme}) => theme.unchangeableColors.black};
+  color: ${({theme}) => theme.unchangeableColors.white};
+  height: 50px;
+  max-width: 100vw;
+`;
