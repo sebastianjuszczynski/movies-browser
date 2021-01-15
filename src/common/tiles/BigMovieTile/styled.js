59ce1d37af2b268a90235d6b71ab47fa0028af80
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-15 18:04:35 +0000

    unnecessary code removed

--- src/common/tiles/BigMovieTile/styled.js
+++ src/common/tiles/BigMovieTile/styled.js
@@ -4,15 +4,12 @@ import { Image } from "./../tilesStyles";
 export const BigMovieImage = styled(Image)`
   float: left;
   object-fit: cover;
-  width: 312px;
   height: 464px;
   margin: 0 40px 16px 0;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.screenSmall}px) {
     margin: 0 25px 6px 0;
-    float: unset;
     height: 316px;
-    width: unset;
   }
   
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
