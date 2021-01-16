5634a7d9fa732902eff2a295d6cb183920ddfb7b
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-01-16 17:32:45 +0000

    height fixed in BigTilePerson

--- src/common/tiles/BigPersonTile/styled.js
+++ src/common/tiles/BigPersonTile/styled.js
@@ -3,19 +3,19 @@ import { Image } from "./../tilesStyles";
 
 export const BigPersonImage = styled(Image)`
   float: left;
-  object-fit: cover;
-  max-height: 564px;
+  object-fit: contain;
+  height: 564px;
   margin-right: 40px;
   
   @media (max-width: ${({ theme }) => theme.breakpoints.screenSmall}px) {
-    max-height: 439px;
+    height: 439px;
     margin-right: 25px;
   }
   @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
-    max-height: 300px;
+    height: 300px;
   }
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    max-height: 163px;
+    height: 163px;
     margin-right: 16px;
   }
 `;
