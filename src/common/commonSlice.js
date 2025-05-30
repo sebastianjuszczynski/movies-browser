59ea864adc0bd30079c72ff206b918232f2cdae5
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2025-05-30 16:58:19 +0200

    refactor: clean up formatting and improve readability in commonSlice.js

--- src/common/commonSlice.js
+++ src/common/commonSlice.js
@@ -1,45 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const commonSlice = createSlice({
-    name: "common",
-    initialState: {
-        genresList: [],
-        loading: true,
-        isNormalTheme: localStorage.getItem("theme")
-        ? JSON.parse(localStorage.getItem("theme"))
-        : true,
+  name: "common",
+  initialState: {
+    genresList: [],
+    loading: true,
+    isNormalTheme: localStorage.getItem("theme")
+      ? JSON.parse(localStorage.getItem("theme"))
+      : true,
+  },
+  reducers: {
+    fetchCommon: (state) => {
+      state.loading = true;
     },
-    reducers: {
-        fetchCommon: state => {
-            state.loading = true;
-        },
-        fetchCommonSuccess: (state, { payload: data }) => {
-            state.genresList = data.genres.map(genre => ({ ...genre, enabled: false }));
-            state.loading = false;
-        },
-        fetchCommonError: state => {
-            state.loading = false;
-        },
-        setTheme: (state, { payload }) => {
-            state.isNormalTheme = payload;
-            localStorage.setItem("theme", payload);
-        },
-        switchGenreEnabled: ({ genresList }, { payload: id }) => {
-            const index = genresList.findIndex(genre => genre.id === id);
-            genresList[index].enabled = !genresList[index].enabled;
-        },
+    fetchCommonSuccess: (state, { payload: data }) => {
+      state.genresList = data.genres.map((genre) => ({
+        ...genre,
+        enabled: false,
+      }));
+      state.loading = false;
     },
+    fetchCommonError: (state) => {
+      state.loading = false;
+    },
+    setTheme: (state, { payload }) => {
+      state.isNormalTheme = payload;
+      localStorage.setItem("theme", JSON.stringify(payload));
+    },
+    switchGenreEnabled: ({ genresList }, { payload: id }) => {
+      const index = genresList.findIndex((genre) => genre.id === id);
+      genresList[index].enabled = !genresList[index].enabled;
+    },
+  },
 });
 
 export const {
-    fetchCommon,
-    fetchCommonSuccess,
-    fetchCommonError,
-    setTheme,
-    switchGenreEnabled,
+  fetchCommon,
+  fetchCommonSuccess,
+  fetchCommonError,
+  setTheme,
+  switchGenreEnabled,
 } = commonSlice.actions;
-export const selectGenres = state => state.common.genresList;
-export const selectLoading = state => state.common.loading;
-export const selectTheme = state => state.common.isNormalTheme;
+
+export const selectGenres = (state) => state.common.genresList;
+export const selectLoading = (state) => state.common.loading;
+export const selectTheme = (state) => state.common.isNormalTheme;
 
 export default commonSlice.reducer;
